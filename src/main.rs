d418c5290975d320dcd9b5958285d59ea972b462
Vyacheslav Moskalev <slouffka@gmail.com>
2019-08-17 04:38:35 +0700

    Routes using doc 1.0 from website.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get};
 
-fn hello() -> impl Responder {
-    format!("Hello World!")
+#[get("/")]
+fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello World!")
 }
 
-fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+#[get("/hello")]
+fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello Lowrd!")
 }
 
 fn main() {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(hello))
-            .route("/{name}", web::get().to(greet))
+            .service(index)
+            .service(hello)
     })
-    .bind("127.0.0.1:3000")
-    .expect("Can not bind to port 3000")
+    .bind("0.0.0.0:3000")
+    .unwrap()
     .run()
     .unwrap();
 }
